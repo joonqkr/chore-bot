6506310d3be21b7232f9104f8619e130bb2c7038
Joon Park <joonqkr@berkeley.edu>
2021-07-05 00:14:54 -0400

    set up Main class

---
+++ chore_bot/Main.java
@@ -0,0 +1,14 @@
+package chore_bot;
+
+/** Driver class for Chore Bot.
+ *  @author Joon Park
+ */
+ public class Main {
+
+     /** Usage: java chore_bot.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND> ....
+     *  java chore_bot.Main */
+     public static void main(String... args) {
+
+     }
+ }
