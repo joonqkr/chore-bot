8dc394b6b739a20a590cf5342a3ad604528585c4
Joon Park <joonqkr@berkeley.edu>
2021-08-15 07:52:02 -0400

    finished house command

--- src/main/Command.java
+++ src/main/Command.java
@@ -1,11 +1,31 @@
 package main;
 
+import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
+
+import static main.ReadWriteObject.*;
 
 public class Command {
 
-    public void init() {
-        
+    public void house() throws IOException {
+        File cwd = new File(System.getProperty("user.dir"));
+        File cb = new File(cwd, ".chore_bot");
+        if (cb.exists()) {
+            System.out.println("A chore system already exists here.");
+            return;
+        }
+        cb.mkdir();
+        File chores = new File(cb, "chores");
+        chores.createNewFile();
+        File persons = new File(cb, "persons");
+        persons.createNewFile();
+
+        ArrayList<Chore> choreList = new ArrayList<Chore>();
+        ArrayList<Person> personList = new ArrayList<Person>();
+
+        writeObject(chores, choreList);
+        writeObject(persons, personList);
     }
 
 }
