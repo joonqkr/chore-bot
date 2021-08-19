5259d9c7eb97c45ead229570bec48edb019aa4fc
Joon Park <joonqkr@berkeley.edu>
2021-08-18 23:58:16 -0400

    updated unit test

--- src/test/ChoreTest.java
+++ src/test/ChoreTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import main.Chore;
+import main.Person;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -11,33 +12,41 @@ public class ChoreTest {
 
     @Test
     public void testChoreAttributes() {
-        Chore washDishes = new Chore("wash dishes", 52, 3, 365);
+        Chore washDishes = new Chore("wash dishes", 52);
         assertEquals("wash dishes", washDishes.getName());
         assertEquals(52, washDishes.getFrequency());
-        assertEquals(3, washDishes.getNumPeople());
-        assertEquals(365, washDishes.getDuration());
+        assertEquals(0, washDishes.getPersons().size());
+        assertNull(washDishes.getCurrent());
         assertTrue(washDishes.getStatus());
 
-        Chore takeTrash = new Chore("take out trash", 26, 1, 180);
+        Chore takeTrash = new Chore("take out trash", 26);
         assertEquals("take out trash", takeTrash.getName());
         assertEquals(26, takeTrash.getFrequency());
-        assertEquals(1, takeTrash.getNumPeople());
-        assertEquals(180, takeTrash.getDuration());
+        assertEquals(0, takeTrash.getPersons().size());
+        assertNull(takeTrash.getCurrent());
         assertTrue(takeTrash.getStatus());
     }
 
     @Test
     public void testChoreMethods() {
-        Chore cleanBathroom = new Chore("clean the bathroom", 24, 3, 200);
+        Chore cleanBathroom = new Chore("clean the bathroom", 24);
         cleanBathroom.setFrequency(26);
         assertEquals(26, cleanBathroom.getFrequency());
         cleanBathroom.updateName("cleaning the bathroom");
         assertEquals("cleaning the bathroom", cleanBathroom.getName());
-        cleanBathroom.updateNumPeople(1);
-        assertEquals(1, cleanBathroom.getNumPeople());
-        cleanBathroom.updateDuration(365);
-        assertEquals(365, cleanBathroom.getDuration());
         cleanBathroom.updateStatus();
         assertFalse(cleanBathroom.getStatus());
+
+        Person joon = new Person("Joon");
+        cleanBathroom.addPerson(joon);
+        Person twong = new Person("Tiff");
+        cleanBathroom.addPerson(twong);
+        cleanBathroom.updateCurrent(joon);
+        assertEquals(2, cleanBathroom.getPersons().size());
+        assertEquals(joon.getName(), cleanBathroom.getCurrent().getName());
+        cleanBathroom.updateCurrent(twong);
+        assertEquals(twong.getName(), cleanBathroom.getCurrent().getName());
+        cleanBathroom.removePerson(joon);
+        assertEquals(1, cleanBathroom.getPersons().size());
     }
 }
