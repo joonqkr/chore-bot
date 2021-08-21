6f7255367c081f1c97a206c79eb4a8eb412b8d7b
Joon Park <joonqkr@berkeley.edu>
2021-08-20 22:34:24 -0400

    resolved class casting conflicts in write method

--- src/main/ReadWriteObject.java
+++ src/main/ReadWriteObject.java
@@ -8,15 +8,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 /** Read and write methods.
  *  @author Joon Park
  */
-class ReadWriteObject {
+public class ReadWriteObject {
 
     /** Return an object of type T read from FILE, casting it to C. */
-    static <T extends Serializable> T readObjectFromFile(File file, Class<T> c) throws IOException, ClassNotFoundException {
+    public static <T extends Serializable> T readObjectFromFile(File file, Class<T> c) throws IOException, ClassNotFoundException {
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
         T obj = c.cast(ois.readObject());
         ois.close();
@@ -24,14 +25,15 @@ class ReadWriteObject {
     }
 
     /** Write OBJ to FILE. */
-    static void writeObjectToFile(File file, Serializable obj) throws IOException {
+    public static void writeObjectToFile(File file, Serializable obj) throws IOException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         ObjectOutputStream objStream = new ObjectOutputStream(byteStream);
         objStream.writeObject(obj);
         objStream.close();
+        byte[] bytes = byteStream.toByteArray();
         BufferedOutputStream buffStream = new BufferedOutputStream(Files.newOutputStream(file.toPath()));
-        for (Object o : byteStream.toByteArray()) {
-            buffStream.write((byte[]) o);
+        for (byte o : bytes) {
+            buffStream.write(o);
         }
         buffStream.close();
     }
