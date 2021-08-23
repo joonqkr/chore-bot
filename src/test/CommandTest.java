553c72c2ac9c97f95030783db2c2f6a4ccc582c1
Joon Park <joonqkr@berkeley.edu>
2021-08-22 23:25:14 -0700

    finished writing test for add/remove chore/person methods

--- src/test/CommandTest.java
+++ src/test/CommandTest.java
@@ -24,7 +24,7 @@ public class CommandTest {
 
     @Test
     public void houseTest() throws IOException, ClassNotFoundException {
-        File dir = tempFolder.newFolder("subfolder");
+        File dir = tempFolder.newFolder("folder");
 
         Command com = new Command();
         com.house(dir);
@@ -39,6 +39,51 @@ public class CommandTest {
         ArrayList<Chore> choreList = readObjectFromFile(chores, ArrayList.class);
         assertEquals(0, choreList.size());
         ArrayList<Person> personList = readObjectFromFile(persons, ArrayList.class);
+        assertEquals(0, personList.size());
+    }
+
+    @Test
+    public void addRemoveTest() throws IOException, ClassNotFoundException {
+        File dir = tempFolder.newFolder("folder");
+
+        Command com = new Command();
+        com.house(dir);
+        File cb = new File(dir, ".chore_bot");
+
+        File chores = new File(cb, "chores");
+        com.addChore(dir, "take out trash", 26);
+        com.addChore(dir, "wash the dishes", 365);
+        com.addChore(dir, "water plants", 52);
+
+        ArrayList<Chore> choreList = readObjectFromFile(chores, ArrayList.class);
+        assertEquals(3, choreList.size());
+        com.removeChore(dir, "wash the dishes");
+        choreList = readObjectFromFile(chores, ArrayList.class);
+        assertEquals(2, choreList.size());
+        for (Chore chore : choreList) {
+            assertTrue(chore.getName().equals("take out trash")||chore.getName().equals("water plants"));
+        }
+        com.removeChore(dir, "take out trash");
+        com.removeChore(dir, "water plants");
+        choreList = readObjectFromFile(chores, ArrayList.class);
+        assertEquals(0, choreList.size());
+
+        File persons = new File(cb, "persons");
+        com.addPerson(dir, "Joon");
+        com.addPerson(dir, "Cindy");
+        com.addPerson(dir, "Tiffany");
+
+        ArrayList<Person> personList = readObjectFromFile(persons, ArrayList.class);
+        assertEquals(3, personList.size());
+        com.removePerson(dir, "Joon");
+        personList = readObjectFromFile(persons, ArrayList.class);
+        assertEquals(2, personList.size());
+        for (Person person : personList) {
+            assertTrue(person.getName().equals("Cindy")||person.getName().equals("Tiffany"));
+        }
+        com.removePerson(dir, "Cindy");
+        com.removePerson(dir, "Tiffany");
+        personList = readObjectFromFile(persons, ArrayList.class);
         assertEquals(0, personList.size());
     }
 }
